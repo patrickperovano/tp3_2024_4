d0eec556161a49d500869605eee7f2d59d5874ca
Mateus Oliveira <mtsodf@gmail.com>
2024-11-04 17:14:25 -0300

    Exercicios 1 e 2

---
+++ exercicio01.py
@@ -0,0 +1,25 @@
+# Exercicio
+"""
+Os dicionários em Python são estruturas de dados que armazenam pares de chave-valor. Eles são bastante utilizados devido à sua flexibilidade e eficiência na busca de valores associados a uma chave específica. 
+Sobre o funcionamento e as características dos dicionários em Python, responda às perguntas abaixo:
+"""
+
+# 1) O que é um dicionário em Python e qual a sua principal utilização?
+"""
+Escreva sua resposta
+"""
+
+# 2) Como você pode adicionar um novo par chave-valor a um dicionário existente? Dê um exemplo de código.
+"""
+Escreva sua resposta
+"""
+
+# 3) Como você pode remover um item de um dicionário? Explique a diferença entre o método del e o método pop().
+"""
+Escreva sua resposta
+"""
+
+# 4) Quais são algumas das principais operações que você pode realizar com dicionários, como acesso a valores, verificação de chaves, etc.?
+"""
+Escreva sua resposta
+"""
---
+++ exercicio02.py
@@ -0,0 +1,24 @@
+# Exercicio
+"""
+Um dicionário é uma estrutura de dados em Python que armazena pares de chave-valor, enquanto uma lista é uma coleção ordenada de elementos. Sobre as principais diferenças entre dicionários e listas, responda às perguntas abaixo:
+"""
+
+# 1) Quais são as principais diferenças entre listas e dicionários em Python?
+"""
+Escreva sua resposta aqui.
+"""
+
+# 2) Quando você deve usar uma lista em vez de um dicionário e vice-versa?
+"""
+Escreva sua resposta aqui.
+"""
+
+# 3) Como você pode iterar sobre os elementos de um dicionário em Python?
+"""
+Escreva sua resposta aqui
+"""
+
+# 4) Dê um exemplo de como você pode adicionar e remover elementos de um dicionário.
+"""
+Escreva sua resposta aqui
+"""
