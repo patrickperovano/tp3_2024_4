62c47143f1dd136e5fb4bd82b95faa91b0d6b46c
Patrick Ribeiro Perovano <53125389+patrickperovano@users.noreply.github.com>
2024-11-28 16:18:10 +0000

    1

--- exercicio01.py
+++ exercicio01.py
@@ -6,20 +6,31 @@ Sobre o funcionamento e as características dos dicionários em Python, responda
 
 # 1) O que é um dicionário em Python e qual a sua principal utilização?
 """
-Escreva sua resposta
+Dicionário é uma estrutura de dados em Python que armazena pares de chave-valor. 
+Ele é utilizado na busca de valores associados a uma chave específica.
 """
 
 # 2) Como você pode adicionar um novo par chave-valor a um dicionário existente? Dê um exemplo de código.
 """
-Escreva sua resposta
+dicionario = {'nome': 'Alice', 'idade': 25}
+dicionario['cidade'] = 'São Paulo'
 """
 
 # 3) Como você pode remover um item de um dicionário? Explique a diferença entre o método del e o método pop().
 """
-Escreva sua resposta
+del: Remove o par chave-valor de um dicionário.
+pop(): Remove e retorna o valor associado à chave especificada.
+
+del meu_dicionario['idade']
+idade = meu_dicionario.pop('idade')
 """
 
 # 4) Quais são algumas das principais operações que você pode realizar com dicionários, como acesso a valores, verificação de chaves, etc.?
 """
-Escreva sua resposta
+Acesso a valores: `meu_dicionario['chave']`
+Verificação de chaves: `'chave' in meu_dicionario`
+Iteração sobre chaves: `for chave in meu_dicionario`
+Obter todas as chaves: `meu_dicionario.keys()`
+Obter todos os valores: `meu_dicionario.values()`
+Obter todos os itens (pares chave-valor): `meu_dicionario.items()`
 """
